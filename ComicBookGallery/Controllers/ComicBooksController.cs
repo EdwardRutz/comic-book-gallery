2eb6cf113bd6f53e1046326a885c40722943d2f5
EdwardRutz <96d7cf74@opayq.com>
2018-03-09 15:56:02 -0500

    Updated Comic Book Detail View - Added design files

--- ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -13,6 +13,20 @@ namespace ComicBookGallery.Controllers
         // Return type is ActionResult
         public ActionResult Detail()
         {
+            //Comic Book Data
+            //Switch from using variables to properties(ViewBag)
+            ViewBag.SeriesTitle = "The Amazing Spiderman";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus's life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+            ViewBag.Artists = new string[]
+            {
+                "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazaba",
+                "Colors: Edgar Delgado",
+                "Letters: Chris Eliopoulos"
+            };
+
             return View();
         }
     }
