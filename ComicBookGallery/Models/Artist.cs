9085a8c4afb5222130c12ddd9d0170e45304be36
EdwardRutz <96d7cf74@opayq.com>
2018-03-12 14:04:23 -0400

    Added Artist model

---
+++ ComicBookGallery/Models/Artist.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery.Models
+{
+    //Artist model shows name and role of artist
+    public class Artist
+    {
+        public string Name { get; set; }
+        public string Role { get; set; }
+    }
+}
--- ComicBookGallery/Models/ComicBook.cs
+++ ComicBookGallery/Models/ComicBook.cs
@@ -7,5 +7,11 @@ namespace ComicBookGallery.Models
 {
     public class ComicBook
     {
+        public int Id { get; set; }     //Used to uniquely identify a comic book
+        public string SeriesTitle { get; set; }     //The series to which the comic book belongs
+        public int IssueNumber { get; set; }        //Comic Book's issue number
+        public string DescriptionHtml { get; set; }     //Comic book's description contains HTML content
+        public Artist[] Artists { get; set; }       //References Artist.cs model
+        public bool Favorite { get; set; }      //Is comic a favorite?
     }
 }
