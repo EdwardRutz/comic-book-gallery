c86b031f5138cfe0fa7e8c8036e506ca3f731b72
EdwardRutz <96d7cf74@opayq.com>
2018-03-12 14:25:01 -0400

    Updated Comic book Model - Added string value to uniquely identify comic book for users - Added file name for each cover image - jpg cover image naming convention is series-title-issuenumber.jpg

--- ComicBookGallery/Models/ComicBook.cs
+++ ComicBookGallery/Models/ComicBook.cs
@@ -13,5 +13,30 @@ namespace ComicBookGallery.Models
         public string DescriptionHtml { get; set; }     //Comic book's description contains HTML content
         public Artist[] Artists { get; set; }       //References Artist.cs model
         public bool Favorite { get; set; }      //Is comic a favorite?
+
+        //Uniquely identify and display a comic book
+        public string DisplayText
+        {
+            //Retrun a string that is a combination of series title and issue number
+            //Make this property read-only by not defining a setter
+            get
+            {
+                return SeriesTitle + " #" + IssueNumber;
+            }
+        }
+
+        //Display cover image
+        //Naming convention for cover images is series-title-issuenumber.jpg
+        //Read only since it does not contain a setter
+        public string CoverImageFileName
+        {
+            get
+            {
+                //Replace spaces in file name with hyphens
+                //Make lowercase
+                return SeriesTitle.Replace(" ", "-")
+                    .ToLower() + "-" + IssueNumber + ".jpg";
+            }
+        }
     }
 }
